91433732f4b3eb99cca7c5003bb891197454b5b4
Barxh <152720025+Barxh@users.noreply.github.com>
2026-06-16 02:20:10 +0200

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.kt
@@ -0,0 +1,21 @@
+class NumMatrix(matrix: Array<IntArray>) {
+    var mx = arrayOf(IntArray(1))
+    init{
+        mx = matrix
+    }
+    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
+        var sum = 0
+        for(i in row1..row2){
+            for(j in col1..col2){
+                sum += mx[i][j]
+            }
+        }
+        return sum
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
