2c44230702f304438e71aafc6aed7e030253e3c4
Barxh <152720025+Barxh@users.noreply.github.com>
2026-06-26 00:54:00 +0200

    Add: insert-new-interval - submission-2

---
+++ Data Structures & Algorithms/insert-new-interval/submission-2.kt
@@ -0,0 +1,25 @@
+class Solution {
+    fun insert(intervals: Array<IntArray>, newInterval: IntArray): Array<IntArray> {
+        if(intervals.isEmpty()) return arrayOf(newInterval)
+        val result = mutableListOf<IntArray>()
+        var i = 0
+        while(i < intervals.size && intervals[i][1] < newInterval[0]){
+            result.add(intervals[i])
+            i++
+        }
+        while(i < intervals.size && 
+        ((intervals[i][0] <= newInterval[0] && intervals[i][1] >= newInterval[0]) ||
+        (intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[1]) ||
+        intervals[i][1] < newInterval[1])){
+            newInterval[0] = minOf(newInterval[0], intervals[i][0])
+            newInterval[1] = maxOf(newInterval[1], intervals[i][1])
+            i++
+        }
+        result.add(newInterval)
+        while(i < intervals.size){
+            result.add(intervals[i++])
+        }
+        return result.toTypedArray()
+
+    }
+}
