6df27c3312c50398993cfea13511144200b9708a
Barxh <152720025+Barxh@users.noreply.github.com>
2026-07-01 08:26:53 +0200

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun countSeniors(details: Array<String>): Int {
+        var count = 0
+        for(detail in details){
+            val age = detail.substring(11, 13).toInt()
+            if(age > 60) count++
+        }
+        return count
+    }
+}
