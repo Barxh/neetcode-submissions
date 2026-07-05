8b7785c79c620618cced15419c229a22521ae95c
Barxh <152720025+Barxh@users.noreply.github.com>
2026-07-05 08:56:40 +0200

    Add: redundant-connection - submission-13

---
+++ Data Structures & Algorithms/redundant-connection/submission-13.kt
@@ -0,0 +1,42 @@
+class Solution {
+    fun findRedundantConnection(edges: Array<IntArray>): IntArray {
+        val nodes = Array(edges.size){mutableListOf<Int>()}
+        for(edge in edges){
+            nodes[edge[0] - 1].add(edge[1])
+            nodes[edge[1] - 1].add(edge[0])
+        }
+        val nodesInLoop = HashSet<Int>()
+        var loopBegin = -1
+        val visitedNodes = BooleanArray(edges.size)
+        fun getNodesInLoop(cur: Int, prev: Int){
+            if(visitedNodes[cur - 1]){
+                loopBegin = cur
+                nodesInLoop.add(cur)
+                return
+            }
+            visitedNodes[cur - 1] = true
+            for(neighbor in nodes[cur - 1]){
+                if(neighbor == prev) continue
+                getNodesInLoop(neighbor, cur)
+                when{
+                    loopBegin == -2 -> {
+                        return
+                    }
+                    loopBegin != -1 ->{
+                        if(loopBegin == cur) {
+                            loopBegin = -2 
+                        }
+                        nodesInLoop.add(cur)
+                        return
+                    }
+                }
+            }
+            visitedNodes[cur - 1] = false
+        }
+        getNodesInLoop(1, -1)
+        for(i in edges.lastIndex downTo 0){
+            if(edges[i][0] in nodesInLoop && edges[i][1] in nodesInLoop) return edges[i]
+        }
+        return intArrayOf()
+    }
+}
